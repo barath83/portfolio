613c2f40e945a27f1f120d73927378c8a6536a25
Barath Gopinath <barathgopi1699@gmail.com>
2020-06-06 15:37:08 +0530

    Updates

---
+++ precache-manifest.e9fc471acce28777699b11ab7e9067c0.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "caba14c3f58d3e0df9024f9ce8e2ca22",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "80915b2394e1d698e4b4",
+    "url": "/portfolio/static/css/main.974609af.chunk.css"
+  },
+  {
+    "revision": "cf5ad3abe81d90c4775f",
+    "url": "/portfolio/static/js/2.7923f6a8.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/portfolio/static/js/2.7923f6a8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "80915b2394e1d698e4b4",
+    "url": "/portfolio/static/js/main.a980309a.chunk.js"
+  },
+  {
+    "revision": "88fbd3d397ae3690d5f2",
+    "url": "/portfolio/static/js/runtime-main.fb1b3dac.js"
+  },
+  {
+    "revision": "a0cb693c715aaf804e67963c4d0d4d90",
+    "url": "/portfolio/static/media/Modeseven-L3n5.a0cb693c.ttf"
+  },
+  {
+    "revision": "972d54ac7985dbc9a8014a49a29c7668",
+    "url": "/portfolio/static/media/ShareTechMono-Regular.972d54ac.ttf"
+  }
+]);
