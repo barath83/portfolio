356bd934e9af9496599b7f735f4374feba471b0f
Barath Gopinath <barathgopi1699@gmail.com>
2020-09-09 14:55:55 +0530

    final 2.0

--- src/components/PageComponents/AboutMe.js
+++ src/components/PageComponents/AboutMe.js
@@ -10,7 +10,7 @@ const aboutMe = props => {
             <h2 className="about-title">About Me</h2>
             <div className="p-div">
                 <p className="about-content">
-                Hey, this is Barath, I'm a pre-final year CS undergrad.I love developing front end applications and devising creative solutions to intricate problems.
+                Hey, this is Barath, I'm a final year CS undergraduate.I love developing front end applications and devising creative solutions to intricate problems.
                 I'm a curious learner and a passion driven person about the things I do. I give my 100% even in getting the minute fragments of the whole process right.
                 <br></br><br></br>
                 Apart from this, given a ticket to travel,I would pack my bag rather than asking for the destination.I am enthralled to watching movies and cricket.So pick a game from history or suggest me a movie,that should do for an interesting conversation!
